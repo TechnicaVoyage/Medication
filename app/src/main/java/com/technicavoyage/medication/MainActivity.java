e86de07dc75b26b8e228a7db90da553afb5b9203
TechnicaVoyage <dhavalat60@gmail.com>
2016-07-21 20:21:34 +0530

    Removed unused imports
    Cleaner workspace

--- app/src/main/java/com/technicavoyage/medication/MainActivity.java
+++ app/src/main/java/com/technicavoyage/medication/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
